7593bfa683f4cdc9b89b44f133d7c5be27325a28
Marisolgc13 <64939683+Marisolgc13@users.noreply.github.com>
2023-03-05 17:29:54 -0700

    Add files via upload

---
+++ EVA1_1_STACK/src/eva1_1_stack/EVA1_1_STACK.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_stack;
+
+/**
+ *
+ * @author Marisol G
+ */
+public class EVA1_1_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Inicia main");
+        A();
+         System.out.println ("Termina Main");
+    }
+    public static void A(){
+        System.out.println ("Inicia A");
+        B();
+         System.out.println ("Termina A");
+    }
+    public static void B(){
+         System.out.println ("Inicia B");
+         int x = 100;
+         double y = 100.0;
+          System.out.println ("Termina B");
+    }
+    }
+
